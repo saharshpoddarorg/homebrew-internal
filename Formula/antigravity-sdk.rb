844438d56d3617ed72c170bde769f42a990628e9
Saharsh Poddar <42889633+saharshpoddar@users.noreply.github.com>
2026-06-22 22:00:51 +0530

    Remove unnecessary blank line in antigravity-sdk.rb

--- Formula/antigravity-sdk.rb
+++ Formula/antigravity-sdk.rb
@@ -2,7 +2,6 @@
 class AntigravitySdk < Formula
   desc "Core development kit for compiling Antigravity modules"
   homepage "https://github.com/saharshpoddarorg/homebrew-internal"
-  
   url "https://github.com/saharshpoddarorg/homebrew-internal-releases/releases/download/v1.0.0/antigravity-sdk-v1.0.0.tar.gz"
   sha256 "251A54E858600B14AA69909A3645F8C58ED272BD38341C744FD386E69B15FD82"
   version "1.0.0"
